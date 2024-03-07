571dab2f1c2d8c9d66a91b7ed41b6f39ac9b0414
Martin Wittemann <martin@wittemann-mail.de>
2024-03-07 21:18:42 +0100

    Add more sections

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,10 +2,10 @@ import { Silkscreen } from 'next/font/google';
 
 const fontHeadline = Silkscreen({ subsets: ['latin'], weight: '700' });
 
-export default function Experience() {
+export default function Projects() {
   return (
     <section>
-      <h2 className={`${fontHeadline.className} mt-12 text-lg`}>Experience</h2>
+      <h2 className={`${fontHeadline.className} mt-12 text-lg`}>Projects</h2>
       <p>TBD</p>
     </section>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import About from '@/components/about';
 import Bio from '@/components/bio';
 import Experience from '@/components/experience';
 import Header from '@/components/header';
+import Projects from '@/components/projects';
 import { Silkscreen } from 'next/font/google';
 
 const fontHeadline = Silkscreen({ subsets: ['latin'], weight: '700' });
@@ -15,7 +17,9 @@ export default function Home() {
       <main className="max-w-screen-xl">
         <Header></Header>
 
+        <About></About>
         <Experience></Experience>
+        <Projects></Projects>
       </main>
     </div>
   );
