47e95021b29a3ff592e3b9c8554f69d984495ab4
Martin Wittemann <martin@wittemann-mail.de>
2024-03-24 16:42:44 +0100

    Minor update

--- components/about.tsx
+++ components/about.tsx
@@ -12,33 +12,28 @@ export default function About() {
         I&apos;m Martin Wittemann – Frontend developer, gamer and proud father.
       </h1>
 
-      <p>
-        Frontend developer and leader with 10+ years of experience in a
-        professional environment in leading and developing of large scale web
-        applications.
-      </p>
-
       <p className="mt-4">
         Somewhere around the year 2000 I started my first webseite as a hobby
-        project which ultimatly lead me to go to the university and study
-        computer science in Karlsruhe, one of the best decisions of my life as
-        computer science and in particular software development is what I
-        learned to love.
+        project which ultimatly lead me to go to my local university in
+        Karlsruhe and study computer science, one of the best decisions of my
+        life as computer science and in particular software development is what
+        I learned to love.
       </p>
 
       <p className="mt-4">
         After a long period of leading frontend developers and only beeing a
-        part time developerI recently swtched back to my roots in Frontend
-        Developoment. Over time, the size of my team and my responsibilites grew
+        part time developer I recently swtched back to my roots in frontend
+        developoment. Over time, the size of my team and my responsibilites grew
         but at the same time, the time spend on coding and solving technical
-        issues shrunk. Now my main focus is back on software engineering where
-        my passion is.
+        issues shrunk. Now my main focus is back where my passion is on software
+        engineering.
       </p>
 
       <p className="mt-4">
-        I do spend most of my spare time with my family which is my main driver.
-        The rest of the time is spend on gaming, doing and watching sports and
-        watching history, physics or computer science videos.
+        I do spend most of my spare time with my family which is my main driver
+        for basically everything. The rest of the time is spend on gaming, doing
+        and watching sports and watching history, physics or computer science
+        videos.
       </p>
     </header>
   );
